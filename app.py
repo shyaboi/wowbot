3e62e41f09ff90c84ab6d73be0ff985aa2c5b438
Shyaboi <ian.shane.sears@gmail.com>
2020-09-06 09:26:48 -0700

    initiazl comit

---
+++ app.py
@@ -0,0 +1,83 @@
+import autopy
+import random
+import time
+
+
+
+def mousLoc():
+    print(autopy.mouse.location())
+    
+def rightJerk():
+    autopy.mouse.move(1200, 500)
+    mousLoc()
+    autopy.mouse.toggle(autopy.mouse.Button.LEFT, True)
+    autopy.mouse.toggle(autopy.mouse.Button.RIGHT, True)
+    autopy.mouse.smooth_move(1270, 500)
+    mousLoc()
+    autopy.mouse.toggle(autopy.mouse.Button.LEFT, False)
+    autopy.mouse.toggle(autopy.mouse.Button.RIGHT, False)
+def leftJerk():
+    autopy.mouse.move(1200, 500)
+    mousLoc()
+    autopy.mouse.toggle(autopy.mouse.Button.LEFT, True)
+    autopy.mouse.toggle(autopy.mouse.Button.RIGHT, True)
+    autopy.mouse.smooth_move(1135, 500)
+    mousLoc()
+    autopy.mouse.toggle(autopy.mouse.Button.LEFT, False)
+    autopy.mouse.toggle(autopy.mouse.Button.RIGHT, False)
+def autoRun():
+    autopy.key.tap(autopy.key.Code.UP_ARROW)
+def jumpy():
+    autopy.key.tap(autopy.key.Code.SPACE)
+
+
+
+def wow():
+    # autopy.mouse.move(600, 600)
+    # autopy.mouse.click()
+    autopy.key.tap(autopy.key.Code.TAB, [autopy.key.Modifier.ALT])
+    time.sleep(1)
+    mousLoc()
+    autoRun()
+    time.sleep(3.5)
+    rightJerk()
+    rightJerk()
+    rightJerk()
+    rightJerk()
+    rightJerk()
+    autoRun()
+    time.sleep(1)
+    rightJerk()
+    autoRun()
+    time.sleep(5)
+    leftJerk()
+    leftJerk()
+    leftJerk()
+    autoRun()
+    jumpy()
+    time.sleep(12)
+    rightJerk()
+    autoRun()
+    jumpy()
+    time.sleep(42)
+    leftJerk()
+    leftJerk()
+    leftJerk()
+    autoRun()
+    jumpy()
+    time.sleep(8)
+    leftJerk()
+    leftJerk()
+    autoRun()
+    jumpy()
+    time.sleep(2)
+    leftJerk()
+    leftJerk()
+    autoRun()
+    jumpy()
+    time.sleep(13)
+    leftJerk()
+    leftJerk()
+    autoRun()
+    jumpy()
+wow()
